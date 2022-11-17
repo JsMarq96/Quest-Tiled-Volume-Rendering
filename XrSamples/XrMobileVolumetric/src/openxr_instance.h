68014c44826559ba1c91383c922d66fa0dcec0b9
Juan S. Marquerie <jsmarquerie@gmail.com>
2022-11-17 13:11:41 +0100

    Finished basic OpenXR integration

--- XrSamples/XrMobileVolumetric/src/openxr_instance.h
+++ XrSamples/XrMobileVolumetric/src/openxr_instance.h
@@ -16,6 +16,73 @@
 #include "egl_context.h"
 #include "app_data.h"
 
+struct sFramebuffer {
+    uint32_t width = 0;
+    uint32_t height = 0;
+    // multisamples
+
+    // SwapChain
+    uint32_t swapchain_length = 0;
+    uint32_t swapchain_index = 0;
+
+    // Color swapchain
+    XrSwapchain swapchain_handle;
+    uint32_t swapchain_width = 0;
+    uint32_t swapchain_height = 0;
+
+    XrSwapchainImageOpenGLESKHR *swapchain_images = NULL;
+
+    uint32_t depth_buffers;
+    uint32_t color_buffers;
+
+    void init(XrSession &session,
+              const uint32_t i_width,
+              const uint32_t i_height,
+              const GLenum   color_format){
+        width = i_width;
+        height = i_height;
+
+        // Skip format verification
+
+        XrSwapchainCreateInfo swapchain_create = {
+                .type = XR_TYPE_SWAPCHAIN_CREATE_INFO,
+                .usageFlags = XR_SWAPCHAIN_USAGE_COLOR_ATTACHMENT_BIT | XR_SWAPCHAIN_USAGE_SAMPLED_BIT,
+                .format = color_format,
+                .sampleCount = 1,
+                .width = width,
+                .height = height,
+                .faceCount = 1, // ??
+                .arraySize = 1,
+                .mipCount = 1
+        };
+
+        // TODO create FOVeationg
+        xrCreateSwapchain(session,
+                          &swapchain_create,
+                          &swapchain_handle);
+
+        // Generate the images for the swapchain
+        // The number
+        xrEnumerateSwapchainImages(swapchain_handle,
+                                   0,
+                                   &swapchain_length,
+                                   NULL);
+        // Allocate
+        swapchain_images = (XrSwapchainImageOpenGLESKHR*) malloc(sizeof(XrSwapchainImageOpenGLESKHR) * swapchain_length);
+        // Fill
+        for(uint32_t i = 0; i < swapchain_length; i++) {
+            swapchain_images[i].type = XR_TYPE_SWAPCHAIN_IMAGE_OPENGL_ES_KHR;
+            swapchain_images[i].next = NULL;
+        }
+        xrEnumerateSwapchainImages(swapchain_handle,
+                                   swapchain_length,
+                                   &swapchain_length,
+                                   (XrSwapchainImageBaseHeader*) swapchain_images);
+
+        // Init textures and FBOs??
+    }
+};
+
 struct sOpenXR_Instance {
     // OpenXR session & context data
     XrInstance xr_instance;
@@ -24,11 +91,11 @@ struct sOpenXR_Instance {
     XrSpace xr_stage_space, xr_local_space;
     sEglContext egl;
 
-
+    XrViewConfigurationView view_configs[MAX_EYE_NUMBER];
     XrView eye_projections[MAX_EYE_NUMBER];
 
 
-    void init() {
+    void init(sFramebuffer *framebuffer) {
         // Load extensions ==============================================================
         uint32_t extension_count = 9;
         const char *enabled_extensions[12] = {
@@ -95,7 +162,62 @@ struct sOpenXR_Instance {
                                &session_info,
                                &xr_session) == XR_SUCCESS && "Failed to create session");
 
-        //const XrViewConfigurationType supported_view_config = XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO;
+        // Load ViewPort data ===============================================================
+        const XrViewConfigurationType stereo_view_config = XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO;
+        uint32_t viewport_count = 0;
+        xrEnumerateViewConfigurations(xr_instance,
+                                      xr_sys_id,
+                                      0,
+                                      &viewport_count,
+                                      NULL);
+
+        assert(viewport_count > 0 && "No enought viewport configs on device");
+
+        XrViewConfigurationType *viewport_configs = (XrViewConfigurationType*) malloc(sizeof(XrViewConfigurationType) * viewport_count);
+        xrEnumerateViewConfigurations(xr_instance,
+                                      xr_sys_id,
+                                      viewport_count,
+                                      &viewport_count,
+                                      viewport_configs);
+
+        for(uint32_t i = 0; i < viewport_count; i++) {
+            const XrViewConfigurationType vp_config_type = viewport_configs[i];
+
+            // Only interested in stereo rendering
+            if (vp_config_type != stereo_view_config) {
+                continue;
+            }
+
+            uint32_t view_count = 0;
+            xrEnumerateViewConfigurationViews(xr_instance,
+                                              xr_sys_id,
+                                              vp_config_type,
+                                              0,
+                                              &view_count,
+                                              NULL);
+            if (view_count <= 0) {
+                continue; // Empty viewport
+            }
+
+            assert(view_count == MAX_EYE_NUMBER && "Only need two eyes on stereo rendering!");
+
+            XrViewConfigurationView *views = (XrViewConfigurationView*) malloc(sizeof(XrViewConfigurationView) * view_count);
+            for(uint32_t j = 0; j < view_count; j++) {
+                views[j].type = XR_TYPE_VIEW_CONFIGURATION_VIEW;
+                views[j].next = NULL;
+            }
+
+            xrEnumerateViewConfigurationViews(xr_instance,
+                                              xr_sys_id,
+                                              vp_config_type,
+                                              view_count,
+                                              &view_count,
+                                              views);
+
+            view_configs[0] = views[0];
+            view_configs[1] = views[1];
+        }
+
 
         // Color space???
 
@@ -142,7 +264,7 @@ struct sOpenXR_Instance {
                                &xr_local_space);
 
         // CONFIG VIEWS ================================================================
-        for(uint16_t i = 0; i < MAX_EYE_NUMBER; i++) {
+        for(uint16_t i = 0; i < MAX_EYE_NUMBER; i++) { // To fill on the realtime
             memset(&eye_projections[i],
                    0,
                    sizeof(XrView));
@@ -153,6 +275,12 @@ struct sOpenXR_Instance {
 
         // Create SWAPCHAIN ===========================================================
 
+        framebuffer->init(xr_session,
+                          view_configs[0].maxImageRectWidth,
+                          view_configs[0].maxImageRectHeight,
+                          GL_SRGB8_ALPHA8);
+
+        // TODO: Foveation
     }
 };
 
