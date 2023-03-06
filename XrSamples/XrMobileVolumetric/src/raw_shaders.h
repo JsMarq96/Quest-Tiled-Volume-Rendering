64a0216e219010102d9117d486fe85f629c94347
Juan S. Marquerie <50079201+JsMarq96@users.noreply.github.com>
2023-03-06 17:04:34 +0100

    Test enviorment

--- XrSamples/XrMobileVolumetric/src/raw_shaders.h
+++ XrSamples/XrMobileVolumetric/src/raw_shaders.h
@@ -110,13 +110,12 @@ in vec2 v_screen_position;
 out vec4 o_frag_color;
 uniform vec3 u_camera_eye_local;
 uniform highp sampler3D u_volume_map;
-const int MAX_ITERATIONS = 100;
-const float STEP_SIZE = 0.01;
+const int MAX_ITERATIONS = 200;
+const float STEP_SIZE = 0.005;
 vec4 render_volume() {
    vec3 ray_dir = normalize(v_local_position - u_camera_eye_local);
    vec3 it_pos = v_local_position + ray_dir * 0.0001; // Start a bit further than the border
    vec4 final_color = vec4(0.0);
-   float ray_step = 1.0 / float(MAX_ITERATIONS);
 
     int i = 0;
     for(; i < MAX_ITERATIONS; i++) {
@@ -131,6 +130,9 @@ vec4 render_volume() {
             break;
         }
         float depth = texture(u_volume_map, it_pos).r;
+        if (0.15 <= depth) {
+            return vec4(it_pos, 1.0);
+        }
         // Increase luminosity, only on the colors
         vec4 sample_color = vec4(04.6 * depth);
         sample_color.a = depth;
@@ -138,7 +140,8 @@ vec4 render_volume() {
         it_pos = it_pos + (STEP_SIZE * ray_dir);
     }
 
-    //return vec4(vec3(i / MAX_ITERATIONS), 1.0);
+    return vec4(vec3(0.0), 1.0);
+    return vec4(it_pos / 2.0 + 0.5, 1.0);
     return vec4(final_color.xyz, 1.0);
 }
 void main() {
@@ -233,10 +236,9 @@ uniform highp sampler3D u_volume_map;
 //uniform highp sampler2D u_albedo_map; // Noise texture
 //uniform highp float u_density_threshold;
 
-const uint MAX_ITERATIONS = 25u;
-const float STEP_SIZE = 0.007; // 0.004 ideal for quality
-const int NOISE_TEX_WIDTH = 100;
-const float DELTA = 0.003;
+const int MAX_ITERATIONS = 100;
+const float STEP_SIZE = 0.50; // 0.004 ideal for quality
+const float DELTA = 0.001;
 const float SMALLEST_VOXEL = 0.0078125; // 2.0 / 256
 
 const vec3 DELTA_X = vec3(DELTA, 0.0, 0.0);
@@ -250,151 +252,96 @@ vec3 gradient(in vec3 pos) {
     return normalize(vec3(x, y, z) / vec3(DELTA * 2.0));
 }
 
-float get_level_of_size(float size) {
+float get_int(in float f) {
+    return float(int(f));
+}
+
+float get_level_of_size(in float size) {
     return 1.0 + log2(size);
 }
-float get_size_of_miplevel(float level) {
-    return round(pow(2.0, level - 1.0));
+float get_size_of_miplevel(in float level) {
+    return get_int(pow(2.0, level - 1.0));
 }
 
 void get_voxel_of_point_in_level(in vec3 point, in float mip_level, out vec3 origin, out vec3 size) {
-    float voxel_count_side = get_size_of_miplevel(mip_level);
-    vec3 voxel_size = vec3(2.0 / voxel_count_side); // The cube is sized 2,2,2 in world coords
+    float voxel_proportions = 2.0 / get_size_of_miplevel(mip_level);// The cube is sized 2,2,2 in world coords
+    vec3 voxel_size = vec3(voxel_proportions);
 
-    vec3 start_coords = round((point + (voxel_size / 2.0)) / voxel_size) * voxel_size;
+    vec3 start_coords = point - mod(point, voxel_proportions);
 
-    origin = start_coords - voxel_size * 0.5;
+    origin = start_coords;
     size = voxel_size;
 }
 
-bool in_the_same_area(in vec3 p1, in vec3 p2, in float mip_level) {
-    float voxel_count_side = get_size_of_miplevel(mip_level);
-    vec3 voxel_size = vec3(2.0 / voxel_count_side); // The cube is sized 2,2,2 in world coords
+bool is_inside(in vec3 box_origin, in vec3 box_size, in vec3 position) {
+    vec3 box_min = box_origin;
+    vec3 box_max = box_origin + box_size;
 
-    vec3 p1_coords = round((p1 + (voxel_size / 2.0)) / voxel_size) * voxel_size;
-    vec3 p2_coords = round((p2 + (voxel_size / 2.0)) / voxel_size) * voxel_size;
-
-    return all(lessThan(p1_coords - p2_coords, vec3(0.001)));
+    return all(greaterThan(position, box_min)) && all(lessThan(position, box_max));
 }
+
 
 float get_distance(in float level) {
-    return pow(2.0, level) * SMALLEST_VOXEL * 0.75;
-}
-
-void ray_AABB_intersection(in vec3 ray_origin,
-                           in vec3 ray_dir,
-                           in vec3 box_origin,
-                           in vec3 box_size,
-                           out vec3 near_intersection,
-                           out vec3 far_intersection) {
-    vec3 box_min = box_origin - (box_size / 2.0);
-    vec3 box_max = box_min + box_size;
-
-    // Testing X axis slab
-    float tx1 = (box_min.x - ray_origin.x) / ray_dir.x;
-    float tx2 = (box_max.x - ray_origin.x) / ray_dir.x;
-    float tmin = min(tx1, tx2);
-    float tmax = max(tx1, tx2);
-
-    // Testing Y axis slab
-    float ty1 = (box_min.y - ray_origin.y) / ray_dir.y;
-    float ty2 = (box_max.y - ray_origin.y) / ray_dir.y;
-    tmin = max(min(ty1, ty2), tmin);
-    tmax = min(max(ty1, ty2), tmax);
-
-    // Testing Z axis slab
-    float tz1 = (box_min.z - ray_origin.z) / ray_dir.z;
-    float tz2 = (box_max.z - ray_origin.z) / ray_dir.z;
-    tmin = max(min(tz1, tz2), tmin);
-    tmax = min(max(tz1, tz2), tmax);
-
-    near_intersection = ray_dir * tmin + ray_origin;
-    far_intersection = ray_dir * tmax + ray_origin;
-    /*if (tmin < tmax) {
-        near_intersection = ray_dir * tmin + ray_origin;
-        far_intersection = ray_dir * tmax + ray_origin;
-    } else {
-         near_intersection = ray_dir * tmax + ray_origin;
-        far_intersection = ray_dir * tmin + ray_origin;
-    }*/
-
+    return 1.0 / get_size_of_miplevel(level) * STEP_SIZE;
+    //return pow(2.0, level - 1.0) * SMALLEST_VOXEL * 0.025;
 }
 
 vec3 mrm() {
     // Raymarching conf
-    vec3 pos = v_local_position;
-    //vec3 ray_dir = normalize(v_local_position - u_camera_eye_local);
-    //vec3 it_pos = v_local_position;
-    //vec3 pos = v_world_position;
-    vec3 ray_dir = normalize(pos - u_camera_eye_local);
-    vec3 it_pos = pos + ray_dir * 0.001;
+    vec3 ray_dir = normalize(v_local_position - u_camera_eye_local);
+    vec3 pos = v_local_position - ray_dir * 0.001;
 
     // MRM
-    uint curr_mipmap_level = 5u;
-    float dist = 0.001; // Distance from start to sampling point
-    const float MAX_DIST = 15.0; // Note, should be the max travel distance of teh ray
+    float curr_mipmap_level = 7.0;
+    float dist = 0.002; // Distance from start to sampling point
     float prev_dist = 0.0;
     vec3 prev_sample_pos = pos;
+    vec3 sample_pos;
 
-    vec3 curr_aabb_origin, curr_aabb_size;
-    vec3 near, far;
+    //return vec3(dist_max);
+    //float prev_mip_level = curr_mipmap_level;
+    vec3 prev_voxel_size = vec3(2.0);
+    vec3 prev_voxel_start = vec3(-1.0);
 
-    ray_AABB_intersection(pos, ray_dir, curr_aabb_origin, curr_aabb_size, near, far);
-    float dist_max = length(near - far);
-
-    uint i = 0u;
+    int i = 0;
     for(; i < MAX_ITERATIONS; i++) {
         vec3 sample_pos = pos + (dist * ray_dir);
-        // Early out, can be skippd
 
-        float depth = textureLod(u_volume_map, sample_pos / 2.0 + 0.5, float(curr_mipmap_level)).r;
-        if (depth > 0.05) { // There is a block
-            //return vec3(1.0, 0.0, 0.0);
-            if (curr_mipmap_level == 0u) {
-                return vec3(1.0);
-            }
-            return vec3(1.0, 0.0, 0.0);
-            curr_mipmap_level--;
-            // compute the AABB
-            get_voxel_of_point_in_level(sample_pos,
-                                        float(curr_mipmap_level),
-                                        curr_aabb_origin,
-                                        curr_aabb_size);
-
-            // Intersect the ray with the AABB
-            ray_AABB_intersection(pos, ray_dir, curr_aabb_origin, curr_aabb_size, near, far);
-
-            // Get near pos
-            dist = max(length(near - pos) + 0.0001, 0.005);
-        } else { // Ray is unblocked
-            if (prev_dist < dist && !in_the_same_area(sample_pos, prev_sample_pos, float(curr_mipmap_level+1u))) {
-                curr_mipmap_level++;
-            } else {
-                dist += get_distance(float(curr_mipmap_level));
-            }
+        // Early out
+        if (!is_inside(vec3(0.0), vec3(1.0), sample_pos)) {
+            break;
         }
 
+        // Go back another level if we are exiting the area for where we came down
+        if (!is_inside(prev_voxel_start, prev_voxel_size, sample_pos)) {
+            curr_mipmap_level = curr_mipmap_level + 1.0;
+            // Recalculate sample position
+            dist = prev_dist;
+            sample_pos = pos + (dist * ray_dir);
+        }
+
+        float depth = textureLod(u_volume_map, sample_pos, curr_mipmap_level).r;
+        if (depth > 0.15) { // There is a block
+            if (curr_mipmap_level == 0.0) {
+                return sample_pos;
+                //break;
+                //return gradient(sample_pos) * 0.5 + 0.5;
+            }
+            get_voxel_of_point_in_level(sample_pos, curr_mipmap_level, prev_voxel_start, prev_voxel_size);
+            curr_mipmap_level = curr_mipmap_level - 1.0;
+            // go back  one step
+            dist = prev_dist;
+        } else { // Ray is unblocked
+            dist += get_distance(curr_mipmap_level);
+        }
         prev_dist = dist;
-        prev_sample_pos = sample_pos;
     }
 
-    //return vec3(i / MAX_ITERATIONS);
+    //return vec3(float(i)  / float(MAX_ITERATIONS));
     return vec3(0.0);
 }
 
 void main() {
-    vec3 ray_origin = v_world_position; //(u_model_mat *  vec4(u_camera_eye_local, 1.0)).rgb;
-    vec3 ray_dir = normalize(ray_origin - u_camera_eye_local);
-   //o_frag_color = texture(u_albedo_map, gl_FragCoord.xy / vec2(NOISE_TEX_WIDTH));
-   //o_frag_color = render_volume(); //*
-   vec3 near, far, box_origin = vec3(0.0, 0.0, 0.0), box_size = vec3(2.0);
-
-   vec3 origin, size;
-   get_voxel_of_point_in_level(v_world_position, 4.0, origin, size);
-
-   ray_AABB_intersection(ray_origin, ray_dir,  origin, size, near, far);
-
-   o_frag_color = vec4(vec3(near), 1.0);
    o_frag_color = vec4(mrm(), 1.0);
 })";
 
